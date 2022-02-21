df96c564e1eeb4df982f824151e493691b82010f
TaherNaqi <eng-naqi@hotmail.com>
2022-02-21 16:58:16 +0300

    delete and update

--- api/trips/trips.controller.js
+++ api/trips/trips.controller.js
@@ -1,4 +1,5 @@
 const res = require("express/lib/response");
+const { findByIdAndDelete } = require("../../models/Trip");
 const Trip = require("../../models/Trip");
 
 exports.fetchTrip = async (tripId, next) => {
@@ -19,8 +20,11 @@ exports.getTrips = async (req, res, next) => {
 };
 exports.createTrip = async (req, res, next) => {
   try {
-    // if (req.file)
-    //   req.body.image = `${req.protocol}://${req.get("host")}/${req.file.path}`;
+    console.log(req.file);
+    if (req.file) {
+      req.body.image = `/${req.file.path}`;
+      req.body.image = req.body.image.replace("\\", "/");
+    }
     // req.body.owner = { id: req.user._id, username: req.user.username };
     const newTrip = await Trip.create(req.body);
     res.status(201).json(newTrip);
@@ -30,21 +34,27 @@ exports.createTrip = async (req, res, next) => {
 };
 exports.updateTrip = async (req, res, next) => {
   try {
-    if (!req.user._id.equals(req.trip.owner._id)) {
-      next({ status: 401, message: "You are not the owner of the trip" });
-    } else {
-      if (req.file) {
-        req.body.image = `${req.protocol}://${req.get("host")}/${
-          req.file.path
-        }`;
-      }
-      const trip = await Trip.findByIdAndUpdate(
-        { _id: req.trip.id, owner: req.trip.owner },
-        req.body,
-        { new: true, runValidators: true }
-      );
-      res.status(201).json(trip);
+    // if (!req.user._id.equals(req.trip.owner._id)) {
+    //   next({ status: 401, message: "You are not the owner of the trip" });
+    // } else {
+    if (req.file) {
+      req.body.image = `/${req.file.path}`;
     }
+    const trip = await Trip.findByIdAndUpdate(
+      { _id: req.trip.id, owner: req.trip.owner },
+      req.body,
+      { new: true, runValidators: true }
+    );
+    res.status(201).json(trip);
+  } catch (error) {
+    next(error);
+  }
+};
+exports.deleteTrip = async (req, res, next) => {
+  try {
+    const { tripId } = req.params;
+    const foundTrip = await Trip.findByIdAndDelete({ _id: tripId });
+    if (foundTrip) res.status(204).end();
   } catch (error) {
     next(error);
   }
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDb = require("./database");
 const cors = require("cors");
+const path = require("path");
 const tripRoutes = require("./api/trips/trips.routes");
 const app = express();
 connectDb();
@@ -11,6 +12,7 @@ app.use((req, res, next) => {
   console.log(`${req.method}://${req.get("host")}${req.originalUrl}`);
   next();
 });
+app.use("/media", express.static(path.join(__dirname, "media")));
 app.use("/api/trips", tripRoutes);
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
